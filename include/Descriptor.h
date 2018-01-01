7bb36a50cbf2168dcf6c630db4cd8a779ca6c1fc
Joshua Weinstein <jweinstein@splunk.com>
2018-01-01 00:17:20 -0800

    started descriptor

---
+++ include/Descriptor.h
@@ -0,0 +1,12 @@
+#ifndef DESCRIPTOR_HEAD
+#define DESCRIPTOR_HEAD
+// header that contains Descriptor class for forming description statements (adjectives)
+
+#include "RandStrList.h"
+
+class Descriptor
+{
+
+};
+
+#endif
