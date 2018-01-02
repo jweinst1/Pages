a0f66f8e16e6fbd3e1966064cd53cdce1ec265d2
Joshua Weinstein <jweinstein@splunk.com>
2018-01-02 15:51:15 -0800

    working on story object

--- old/StoryElems.h
+++ old/StoryElems.h
@@ -40,38 +40,6 @@ const BlockStr& getName(void) const
 }
 };
 
-class StoryObject
-{
-private:
-RandStrList _dets;
-RandStrList _desc;
-BlockStr _name;
-public:
-StoryObject(const char* name);
-~StoryObject()
-{
-}
 
-void pushDescriptor(const char* string)
-{
-        _desc.push(string);
-}
-
-void pushDeterminer(const char* string)
-{
-        _dets.push(string);
-}
-
-bool hasDeterminers(void) const
-{
-        return _dets.isEmpty();
-}
-
-bool hasDescriptors(void) const
-{
-        return _desc.isEmpty();
-}
-
-};
 
 #endif
